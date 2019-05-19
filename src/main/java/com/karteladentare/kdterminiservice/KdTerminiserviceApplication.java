855d10598165aceb19eda73080c50d53885f88c3
Granit Krasniqi <krasniqigranit59@gmail.com>
2019-05-19 15:01:12 +0200

    first commit

---
+++ src/main/java/com/karteladentare/kdterminiservice/KdTerminiserviceApplication.java
@@ -0,0 +1,13 @@
+package com.karteladentare.kdterminiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdTerminiserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KdTerminiserviceApplication.class, args);
+    }
+
+}
